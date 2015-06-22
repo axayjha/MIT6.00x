834579c74a8e02ebf500522026f521fa7fbba110
Akshay Jha <akshayjha@live.in>
2015-06-22 11:32:11 +0530

    Counting Vowels

---
+++ PSet1Q1.py
@@ -0,0 +1,8 @@
+## Counting Vowels
+
+counter=0
+for letter in s:
+    if  letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+        counter += 1
+print("Number of vowels: " + str(counter))
+        
